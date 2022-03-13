aba353adc92a124998b0df984ea6a6c78214f584
GitMulC <cmulvo@gmail.com>
2022-03-13 21:17:23 +0000

    Add test & js script files

---
+++ scripts/tests/game.test.js
@@ -0,0 +1,20 @@
+/**
+ * @jest-environment jsdom
+ */
+const { beforeAll, test, expect } = require("@jest/globals");
+const { describe } = require("yargs");
+const { game } = require("../game");
+
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+});
