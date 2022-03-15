7849e6a918220517ab8073f8a073ee95c1be971b
GitMulC <cmulvo@gmail.com>
2022-03-15 18:23:27 +0000

    Add final tests

--- scripts/game.js
+++ scripts/game.js
@@ -2,6 +2,8 @@ let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
+    lastButton: "",
+    turnInProgress: false,
     choices: ["button1", "button2", "button3", "button4"],
 }
 
@@ -12,10 +14,12 @@ function newGame() {
     for (let circle of document.getElementsByClassName("circle")) {
         if (circle.getAttribute("data-listener") !== "true") {
             circle.addEventListener("click", (e) => {
+                if (game.currentGame.length > 0 && !game.turnInProgress) {
                 let move = e.target.getAttribute("id");
+                game.lastButton = move;
                 lightsOn(move);
                 game.playerMoves.push(move);
-                playerTurn();
+                playerTurn();}
             });
             circle.setAttribute("data-listener", "true");
         }
@@ -42,14 +46,30 @@ function lightsOn(circ) {
 }
 
 function showTurns() {
+    game.turnInProgress = true;
     game.turnNumber = 0;
     let turns = setInterval(() => {
         lightsOn(game.currentGame[game.turnNumber]);
         game.turnNumber++;
         if (game.turnNumber >= game.currentGame.length) {
             clearInterval(turns);
+            game.turnInProgress = false;
         }
     }, 800);
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
+function playerTurn() {
+    let i = game.playerMoves.length = 1;
+    if (game.currentGame[i] === game.playerMoves[i]) {
+        if (game.currentGame.length === game.playerMoves.length){
+            game.score++;
+            showScore();
+            addTurn();
+        }
+    } else {
+        alert("Wrong move!");
+        newGame();
+    }   
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn };
